5257fb14017282bde2f732d899da52bac7843e7c
CesarMC <cesar_mejia_99@hotmail.com>
2025-03-17 12:14:41 -0500

    agregando proyecto carrito-productos

--- src/components/ProyectCard.tsx
+++ src/components/ProyectCard.tsx
@@ -110,22 +110,34 @@ export const ProyectCard = ({ proyect }: ProyectCardProps) => {
     const [activeIndex, setActiveIndex] = useState<Proyect['id'] | null>(null)
     const [isOverflowing, setIsOverflowing] = useState(false)
 
+    const [contentHeight, setContentHeight] = useState<number | null>(null)
+
     const paragraphRef = useRef<HTMLParagraphElement>(null)
+
+    // useEffect(() => {
+    //     if (paragraphRef.current) {
+    //         setIsOverflowing(
+    //             paragraphRef.current.scrollHeight >
+    //                 paragraphRef.current.clientHeight
+    //         )
+    //     }
+    // }, [])
 
     useEffect(() => {
         if (paragraphRef.current) {
-            setIsOverflowing(
-                paragraphRef.current.scrollHeight >
-                    paragraphRef.current.clientHeight
-            )
+            const { scrollHeight, clientHeight } = paragraphRef.current
+            setIsOverflowing(scrollHeight > clientHeight)
+            setContentHeight(scrollHeight) // Guardamos la altura real del contenido
         }
-    }, [])
+    }, [proyect.description]) // Se actualiza si la descripción cambia
 
     const handleClick = (id: Proyect['id']) => {
         if (isOverflowing) {
             setActiveIndex(activeIndex === id ? null : id)
         }
     }
+
+    const isCollapsed = activeIndex !== proyect.id
 
     return (
         <article className='transition-transform duration-700 shadow-lg rounded-xl overflow-hidden shadow-azul-marino'>
@@ -165,13 +177,13 @@ export const ProyectCard = ({ proyect }: ProyectCardProps) => {
                                 md:items-center '
                 >
                     {/* Descripción con expansión */}
-                    <p
+                    {/* <p
                         ref={paragraphRef}
                         className={`text-celeste text-lg overflow-hidden transition-all duration-300 
                             ${
                                 activeIndex === proyect.id
                                     ? 'h-[14rem] md:h-[7rem]'
-                                    : 'h-[5.5rem] line-clamp-3'
+                                    : 'h-full line-clamp-3'
                             } 
                             ${
                                 isOverflowing
@@ -182,6 +194,26 @@ export const ProyectCard = ({ proyect }: ProyectCardProps) => {
                         onClick={() => handleClick(proyect.id)}
                     >
                         {proyect.description}
+                    </p> */}
+
+                    <p
+                        ref={paragraphRef}
+                        className={`text-celeste text-lg overflow-hidden transition-all duration-300 ease-in-out
+                            ${
+                                isOverflowing
+                                    ? 'cursor-pointer hover:opacity-80'
+                                    : ''
+                            } 
+                            xl:text-xl ${isCollapsed ? 'line-clamp-3' : ''}`}
+                        style={{
+                            height: isCollapsed
+                                ? '5.5rem'
+                                : `${contentHeight}px`,
+                        }}
+                        onClick={() => handleClick(proyect.id)}
+                    >
+                        {proyect.description}
+                        {isCollapsed && isOverflowing && '...'}
                     </p>
 
                     <div className='flex flex-col items-center justify-center gap-4'>
--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -4,6 +4,21 @@ import { tecnologics } from './tecnologics'
 export const proyects: Proyect[] = [
     {
         id: 1,
+        name: 'Tienda de productos',
+        img: './img-proyectos/tienda-ts.png',
+        github: 'https://github.com/CesarMC99/tienda-ts',
+        link: 'https://tienda-ts-pi.vercel.app/',
+        description:
+            'He desarrollado un carrito de compras que, si bien aún necesita mejoras en el aspecto visual, ya tiene una lógica casi completa. Utilicé Zod y TypeScript para validar los datos obtenidos de la API, asegurando que la información de los productos sea confiable. Además, implementé React-Hook-Form junto con Zod para validar el formulario de pago, garantizando que los datos ingresados por el usuario sean correctos antes de procesar la compra. Para la gestión del estado global, empleé Zustand, dividiendo el estado en diferentes slices según la lógica de cada módulo, lo que permite una mejor organización y mantenimiento del código.',
+        tecnologics: [
+            tecnologics[0],
+            tecnologics[3],
+            tecnologics[5],
+            tecnologics[4],
+        ],
+    },
+    {
+        id: 2,
         name: 'Pokedex',
         img: './img-proyectos/pokedex.png',
         github: 'https://github.com/CesarMC99/pokedex-v2',
@@ -18,7 +33,7 @@ export const proyects: Proyect[] = [
         ],
     },
     {
-        id: 2,
+        id: 3,
         name: 'Recetas de cócteles',
         img: './img-proyectos/coctel-proyect.png',
         github: 'https://github.com/CesarMC99/buscador-bebidas/tree/main',
@@ -33,7 +48,7 @@ export const proyects: Proyect[] = [
         ],
     },
     {
-        id: 3,
+        id: 4,
         name: 'Tienda de guitarras',
         img: './img-proyectos/tienda-guitarra.png',
         github: 'https://github.com/CesarMC99/carrito-compras-guitarras/tree/main',
@@ -48,7 +63,7 @@ export const proyects: Proyect[] = [
         ],
     },
     {
-        id: 4,
+        id: 5,
         name: 'Buscador de clima',
         img: './img-proyectos/clima-proyect.png',
         github: 'https://github.com/CesarMC99/buscador-clima/tree/main',
